75a40f4cb3a8d08de9f3d5f32ed90e8b9bc3b7f0
dburgos <david.burgos.risco@gmail.com>
2021-11-05 23:27:09 +0100

    Update dependencies and handle timeout

--- src/checkUrl.js
+++ src/checkUrl.js
@@ -4,7 +4,8 @@ const randomItem = require('array-random-item');
 
 module.exports = async function (url, options = {}) {
   const requestConfig = {
-    headers: { 'User-Agent': randomItem(userAgents) }
+    headers: { 'User-Agent': randomItem(userAgents) },
+    timeout: 5 * 1000
   };
   let start;
   let responseTime;
@@ -13,19 +14,26 @@ module.exports = async function (url, options = {}) {
   try {
     start = new Date().getTime();
     const out = await axios.head(url, requestConfig).then((res) => res);
-    responseTime = (new Date().getTime()-start)/1000;
+    responseTime = (new Date().getTime() - start) / 1000;
     isOK = out && (out.status === 200 || out.statusText === 'OK');
     status = out.status;
-    if (!isOK) {
-      start = new Date().getTime();
-      await axios.get(url, requestConfig).then((res) => res.data);
-      responseTime = (new Date().getTime()-start)/1000;
-    }
   } catch (e) {
-    responseTime = (new Date().getTime()-start)/1000;
+    responseTime = (new Date().getTime() - start) / 1000;
     isOK = false;
     status = e && e.response && e.response.status || 500;
   }
+  if (!isOK) {
+    try {
+      start = new Date().getTime();
+      await axios.get(url, requestConfig).then((res) => res.data);
+      responseTime = (new Date().getTime() - start) / 1000;
+    } catch (e) {
+      responseTime = (new Date().getTime() - start) / 1000;
+      isOK = e.code === 'ECONNABORTED' || false;
+      status = e && e.response && e.response.status || 500;
+    }
+  }
+
   const output = {
     url,
     status,
--- src/checkUrl.spec.js
+++ src/checkUrl.spec.js
@@ -1,6 +1,8 @@
 const checkUrl = require('./checkUrl')
 
 describe('checkUrl()', () => {
+  jest.setTimeout(12000)
+
   it('is defined', () => {
     expect(checkUrl).toBeDefined();
     expect(typeof checkUrl).toEqual('function');
@@ -42,21 +44,39 @@ describe('checkUrl()', () => {
     }
   });
 
-	it('check a not found url', async () => {
-		const URL = 'https://glasslytics.com/';
-		let out;
-		let error;
-		try {
-			out = await checkUrl(URL);
-		} catch (e) {
-			error = e;
-		} finally {
-			expect(error).toBeUndefined();
-			expect(out).toBeDefined();
-			expect(out.url).toEqual(URL);
-			expect(out.status).toEqual(500);
-			expect(out.isOK).toEqual(false);
-			expect(out.responseTime).toBeGreaterThan(0);
-		}
-	});
+  it('check a not found url', async () => {
+    const URL = 'https://glasslytics.com/';
+    let out;
+    let error;
+    try {
+      out = await checkUrl(URL);
+    } catch (e) {
+      error = e;
+    } finally {
+      expect(error).toBeUndefined();
+      expect(out).toBeDefined();
+      expect(out.url).toEqual(URL);
+      expect(out.status).toEqual(500);
+      expect(out.isOK).toEqual(false);
+      expect(out.responseTime).toBeGreaterThan(0);
+    }
+  });
+
+  it('deal with a timeout as valid', async () => {
+    const URL = 'https://www.seattletimes.com/business/amazon/amazon-posts-record-sales-and-profit-with-no-slowing-expected/';
+    let out;
+    let error;
+    try {
+      out = await checkUrl(URL);
+    } catch (e) {
+      error = e;
+    } finally {
+      expect(error).toBeUndefined();
+      expect(out).toBeDefined();
+      expect(out.url).toEqual(URL);
+      expect(out.status).toEqual(500);
+      expect(out.isOK).toEqual(true);
+      expect(out.responseTime).toBeGreaterThan(0);
+    }
+  });
 });
